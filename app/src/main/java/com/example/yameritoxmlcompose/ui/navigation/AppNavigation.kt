3bee22d3ab8a6d67963f29519ce83f6686b3fcba
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-09-25 03:01:11 -0500

    Navegación copiada de YaMerito

--- app/src/main/java/com/example/yameritoxmlcompose/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/yameritoxmlcompose/ui/navigation/AppNavigation.kt
@@ -1,9 +1,11 @@
 package com.example.yameritoxmlcompose.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.yameritoxmlcompose.ui.screens.confirm.ui.ConfirmScreen
 import com.example.yameritoxmlcompose.ui.screens.main.ui.MainScreen
 import com.example.yameritoxmlcompose.ui.screens.main.ui.MainViewModel
@@ -16,9 +18,12 @@ fun AppNavigation() {
             MainScreen(navController, mainViewModel = MainViewModel())
         }
         composable(
-            route = AppScreens.ConfirmScreen.route,
+            route = AppScreens.ConfirmScreen.route + "/{text}",
+            arguments = listOf(navArgument(name = "text") {
+                type = NavType.StringType
+            })
         ) {
-            ConfirmScreen(navController)
+            ConfirmScreen(navController, confirmViewModel = ConfirmViewModel(),it.arguments?.getString("text"))
         }
     }
 }
