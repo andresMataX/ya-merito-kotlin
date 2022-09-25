7f319bbf6cd1f3695dfb8af8131f06d3b418ea0c
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-09-24 23:13:23 -0500

    Uso de Composable Fun en XML

--- app/src/main/java/com/example/yameritoxmlcompose/MainActivity.kt
+++ app/src/main/java/com/example/yameritoxmlcompose/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.yameritoxmlcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.ui.platform.ComposeView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val hello = findViewById<ComposeView>(R.id.hello)
+        hello.setContent {
+            Text(text = "El pepe")
+        }
     }
+}
+
+@Composable
+fun Hello() {
+    Text(text = "El pepe")
 }
